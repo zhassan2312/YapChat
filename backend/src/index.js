a7e2bdb58f88e403094893bf5e91bbdb47f12048
zhassan2312 <zhassan.ce44ceme@student.nust.edu.pk>
2025-04-28 16:04:33 +0500

    deployment fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,10 +25,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
-    
     });
 }
 
